831e2f5b960836b620660a9b00950e4a212af77e
CharlesKibet <charlesbiegon973@gmail.com>
2024-03-24 22:24:28 +0300

    Complete lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
-// Write your code here
+// // Write your code here
+let num1 = 2
+let num2 = 31
+let multiply = num1*num2
+ 
+
+const random = Math.round(Math.random() > 0);
+  console.log(random)
+
+  let num3=34
+let num4=5
+let mod = num3%num4
+
+const max = Math.max(1,2,3,20);
+  console.log(max);
